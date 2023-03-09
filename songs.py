2059f0aa27f849df1273fca1b3e4e95393b0b0b2
Lucas Kenji <lkenji.magalhaes@gmail.com>
2023-03-08 23:00:20 -0500

    Update songs.py

--- songs.py
+++ songs.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
+import subprocess
 
-# # Installation & API connection & Libraries
+# Install the required package
+subprocess.call(['pip', 'install', 'spotipy'])
 
+# Import the package after it has been installed
+import spotipy
 
 # Import libraries
 import spotipy
